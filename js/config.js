16e2705ea18ca17c48b50f3f2af1ac8d7b9e4c29
mathieucarbou <mathieu.carbou@gmail.com>
2013-10-24 19:25:35 -0400

    new environments

--- js/config.js
+++ js/config.js
@@ -1,20 +1,21 @@
 //array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
 
-    'm775876286-6205a2ea507d81a05cc8f244', // www
-    'm775876288-3b2dd2c66bbb124ae3c5d4c0', // dashboard
-    'm775876291-4b97ea01fa13aa493c573ffc', // admin
-    'm775876296-2317e7d92e01bfd5d3ea189a', // api
-
-    'm775876285-3e58cd79080b93f250f2d7e2', // www-stg
-    'm775876290-895130656fe4fa55f5516ef6', // dashboard-stg
-    'm775876293-359c6fddf08b0b89eb8354a7', // admin-stg
-    'm775876297-7de464eaa5116cc7ee82b63e', // api-stg
-
     'm775920217-4b9b2ed520abaab7f9b26774', // www-dev
+    'm775876285-3e58cd79080b93f250f2d7e2', // www-stg
+    'm775876286-6205a2ea507d81a05cc8f244', // www
+
     'm775920220-338c12cd44c6fa2e867a1461', // dashboard-dev
+    'm775876290-895130656fe4fa55f5516ef6', // dashboard-stg
+    'm775876288-3b2dd2c66bbb124ae3c5d4c0', // dashboard
+
     'm775920218-89c131df395d012644e29d1d', // admin-dev
+    'm775876293-359c6fddf08b0b89eb8354a7', // admin-stg
+    'm775876291-4b97ea01fa13aa493c573ffc', // admin
+
     'm775920221-d7528628ce76da874c02c848', // api-dev
+    'm775876297-7de464eaa5116cc7ee82b63e', // api-stg
+    'm775876296-2317e7d92e01bfd5d3ea189a', // api
 
     'm775876232-c120b10ca6743f221a4712af', // build
     'm775876282-96c62e51c3d533407662c6e0', // nexus
