2f6b9cb4b4d272db649785891137f2246ae96ce7
mathieucarbou <mathieu.carbou@gmail.com>
2013-11-19 16:14:28 -0500

    added error page

---
+++ error_files/config.js
@@ -0,0 +1,31 @@
+var config = {
+    api: {
+        host: '//guestful.com/',
+        token: {
+            web: 'B0v8rCQZqxl2Kcc62YIrvQ',
+            widget: 'aUnYfEo0yqzfj6qR-ZgfkQ',
+            fbwidget: '9kBFP4SNkx9d6YANmbsbng'
+        }
+    },
+    proxy: {
+        host: '//guestful.com/proxies'
+    },
+    static: {
+        host: '//guestful.com'
+    },
+    dashboard: {
+        host: '//dashboard.guestful.com'
+    },
+    facebook: {
+        appId: '501089929969199',
+        host: '//guestful.com/'
+    },
+    mixpanel: {
+        widget : {
+            token: '22199df14c788568f8bec4989503190e'
+        },
+        onboarding: {
+            token: 'd2814201e46acc87b1d170a4c56d70e3'
+        }
+    }
+};
